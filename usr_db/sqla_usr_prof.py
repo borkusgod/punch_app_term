5ba840efa332d7f7e030d9ac44ea7b1d3d978345
borkusgod <lightraven.bourque@gmail.com>
2023-05-19 23:57:55 -0300

    still some issues with the time class but I want to commit because it's getting late here.

--- usr_db/sqla_usr_prof.py
+++ usr_db/sqla_usr_prof.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, ForeignKey, Column, String, Integer, Boolean
+from sqlalchemy import create_engine, ForeignKey, Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -16,18 +16,19 @@ class Person(BaseMember):
     email = Column("email_address", String)
     phone_number = Column("phone_number", String)
 
-    def __int__(self,
-                ssn,
-                first_n,
-                last_n,
-                gender,
-                age,
-                email,
-                phone_number):
+    def __init__(self,
+                 ssn,
+                 first_n,
+                 last_n,
+                 gender,
+                 age,
+                 email,
+                 phone_number):
         self.ssn = ssn
         self.firstname = first_n
         self.lastname = last_n
         self.gender = gender
+        self.age = age
         self.email = email
         self.phone_number = phone_number
 
@@ -40,7 +41,7 @@ class Person(BaseMember):
 class TimePunch(BaseMember):
     __tablename__ = "time punches"
 
-    tc_related = Column("time punch related", Boolean, primary_key=True)
+    tc_related = Column("time punch related", String, primary_key=True)
     description = Column("Description", String)
     owner = Column(Integer, ForeignKey("user.ssn"))
 
@@ -64,25 +65,25 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 # testing samples
-p1 = Person(139555666, "Bill", "Birdie", "Male", 46)
-p2 = Person(169555888, "Bobbie", "Jenkins", "Male", 76)
-p3 = Person(555444333, "Rhonda", "Boork", "Female", 45)
-p4 = Person(989755187, "Derek", "Dongster", "Male", 55)
+p1 = Person(139555666, "Bill", "Birdie", "Male", 46, "any_given@gmail.com", "555-555-9111")
+p2 = Person(169555888, "Bobbie", "Jenkins", "Male", 76, "a_random@yahoo.com", "888-555-666")
+p3 = Person(555444333, "Rhonda", "Boork", "Female", 45, "free_micro@outlook.com", "999-333-1478")
+p4 = Person(989755187, "Derek", "Dongster", "Male", 55, "free_timewarner@timewarner.com", "187-589-5656")
 
-t1 = TimePunch(True, "day start", p1.ssn)
-t2 = TimePunch(False, "lunch out", p1.ssn)
-t3 = TimePunch(True, "lunch in", p1.ssn)
-t4 = TimePunch(False, "end of day punch out", p1.ssn)
+# t1 = TimePunch("True", "day start", p1.ssn)
+# t2 = TimePunch("False", "lunch out", p1.ssn)
+# t3 = TimePunch("True", "lunch in", p1.ssn)
+# t4 = TimePunch("False", "end of day punch out", p1.ssn)
 
 session.add(p1)
 session.add(p2)
 session.add(p3)
 session.add(p4)
 
-session.add(t1)
-session.add(t2)
-session.add(t3)
-session.add(t4)
+# session.add(t1)
+# session.add(t2)
+# session.add(t3)
+# session.add(t4)
 
 session.commit()
 
@@ -90,12 +91,9 @@ session.commit()
 res_all_peeps = session.query(Person).all()
 print(res_all_peeps)
 
-res_all_things = session.query(TimePunch).all()
-print(res_all_things)
-
-filter_pnames = session.query(Person).filter(Person.age > 45)
-for each in filter_pnames:
-    print(each)
-
-
-
+# res_all_things = session.query(TimePunch).all()
+# print(res_all_things)
+#
+# filter_pnames = session.query(Person).filter(Person.age > 45)
+# for each in filter_pnames:
+#     print(each)
